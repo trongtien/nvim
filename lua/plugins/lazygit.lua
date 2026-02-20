8f89ced66a7b02d30daa49acee713f128f8c4a27
tienplt <phanletrongtien@gmail.com>
2026-02-20 18:45:46 +0700

    feat: Config lazy git

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,18 @@
+return {
+  "kdheepak/lazygit.nvim",
+  lazy = true,
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  -- Optional for floating window border decoration
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  keys = {
+    { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+  },
+}
