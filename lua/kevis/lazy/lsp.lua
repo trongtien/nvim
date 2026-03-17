b0bb38ffc3a745a8a5224083979244d5bcafe7af
tienplt <phanletrongtien@gmail.com>
2026-03-17 22:50:32 +0700

    feat: Update lsp show message

--- lua/kevis/lazy/lsp.lua
+++ lua/kevis/lazy/lsp.lua
@@ -188,5 +188,17 @@ return {
             "bashls",
             "asm_lsp",
         })
+
+        vim.diagnostic.config({
+            -- update_in_insert = true,
+            float = {
+                focusable = false,
+                style = "minimal",
+                border = "rounded",
+                source = "always",
+                header = "",
+                prefix = "",
+            },
+        })
     end
 }
